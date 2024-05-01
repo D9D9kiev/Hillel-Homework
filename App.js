c73bcf84b1e3e9abcb45cc74cf2d3b8007e13421
Dmytro Podmazko <ipodmazko@gmail.com>
2024-05-01 15:35:03 +0300

    hw2

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import Item from "./src/Pages/Home/components/Item";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Hillel</Text>
+      <Text>
+        <Item />
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    margin: "0 auto",
   },
 });
